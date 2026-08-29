73a893acbc9420be6dd3c9d8b41d84885138a58d
adi herlambang <adryfia@gmail.com>
2026-08-29 19:08:01 +0800

    include region data in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
           $this->call([
               MasterReferenceSeeder::class,
               RegistrationPathSeeder::class,
+	      RegionSeeder::class,
         ]);
     }
 }
